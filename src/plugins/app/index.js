ee0e52ae02573b870348a8494ee904a66d0db961
hh54188 <252438823@qq.com>
2018-09-10 16:31:55 +0800

    update

--- src/plugins/app/index.js
+++ src/plugins/app/index.js
@@ -21,42 +21,56 @@ module.exports = {
 
     server.route({
       method: "GET",
-      path: "/app/{param*}",
+      path: `/app/{param*}`,
       handler: defaultHandler(nextRenderService)
     });
 
     server.route({
       method: "GET",
-      path: `/_next/{param*}`,
-      handler: nextHandlerWrapper(nextRenderService)
+      path: `/app/${assetPrefix}/_next/{param*}`,
+      handler: async ({ raw, url }, h) => {
+        const handler = nextRenderService.getRequestHandler();
+        const {
+          pathname: originPathname,
+          path: originPath,
+          href: originHref
+        } = url;
+        const newURL = {
+          ...url,
+          pathname: originPathname.replace("/app/assetPrefix", ""),
+          path: originPath.replace("/app/assetPrefix", ""),
+          href: originHref.replace("/app/assetPrefix", "")
+        };
+        await handler(raw.req, raw.res, newURL);
+        return h.close;
+      }
     });
 
-    // server.route({
-    //   method: "GET",
-    //   path: `/app/${assetPrefix}/_next/on-demand-entries-ping`,
-    //   handler: nextHandlerWrapper(nextRenderService)
-    // });
+    server.route({
+      method: "GET",
+      path: `/app/${assetPrefix}/_next/webpack-hmr`,
+      handler: async ({ raw, url }, h) => {
+        const handler = nextRenderService.getRequestHandler();
+        const {
+          pathname: originPathname,
+          path: originPath,
+          href: originHref
+        } = url;
+        const newURL = {
+          ...url,
+          pathname: originPathname.replace("/app", ""),
+          path: originPath.replace("/app", ""),
+          href: originHref.replace("/app", "")
+        };
+        await handler(raw.req, raw.res, newURL);
+        return h.close;
+      }
+    });
 
-    // server.route({
-    //   method: "GET",
-    //   path: `/app/${assetPrefix}/_next/-/page/{param*}`,
-    //   handler: {
-    //     directory: {
-    //       path: path.join(__dirname, pagesPath),
-    //       listing: true
-    //     }
-    //   }
-    // });
-
-    // server.route({
-    //   method: "GET",
-    //   path: `/app/${assetPrefix}/_next/{param*}`,
-    //   handler: {
-    //     directory: {
-    //       path: path.join(__dirname, distPath),
-    //       listing: true
-    //     }
-    //   }
-    // });
+    server.route({
+      method: "GET",
+      path: `/app/${assetPrefix}/_next/on-demand-entries-ping`,
+      handler: nextHandlerWrapper(nextRenderService)
+    });
   }
 };
